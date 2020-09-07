5154012b4e65ea8227a980f1b62e20136d0258bb
juan12fme <muchomedina.-23@hotmail.com>
2020-09-07 13:14:09 -0500

    subsetting the data on the specific date

--- plot1.R
+++ plot1.R
@@ -2,3 +2,5 @@
 HPCdata<-read.table("household_power_consumption.txt", skip = 1, sep = ";")
 #give name to columns
 names(HPCdata)<- c("Date", "Time", "Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+#subsetting the data to the date we want to plot
+SubHPCdata<- subset(HPCdata, HPCdata$Date=="1/2/2007"|HPCdata$Date=="2/2/2007")
