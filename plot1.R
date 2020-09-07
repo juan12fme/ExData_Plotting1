c6de52c52512d8160028f8d2392266b454c78c69
juan12fme <muchomedina.-23@hotmail.com>
2020-09-07 13:10:30 -0500

    name to the cols

--- plot1.R
+++ plot1.R
@@ -1,4 +1,4 @@
 #read the data
 HPCdata<-read.table("household_power_consumption.txt", skip = 1, sep = ";")
 #give name to columns
-
+names(HPCdata)<- c("Date", "Time", "Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
