59622fd313d603e0dd9065194be65dbc4f08f0ba
juan12fme <muchomedina.-23@hotmail.com>
2020-09-07 23:59:42 -0500

    plot2 created and tested

--- plot1.R
+++ plot1.R
@@ -6,3 +6,4 @@ names(HPCdata)<- c("Date", "Time", "Global_active_power","Global_reactive_power"
 SubHPCdata<- subset(HPCdata, HPCdata$Date=="1/2/2007"|HPCdata$Date=="2/2/2007")
 #plotting
 hist (as.numeric(as.character(SubHPCdata$Global_active_power)), col="red", main = "Global Active Power", xlab = "Global Active Power (Kilowatts)")
+
---
+++ plot2.R
@@ -0,0 +1,19 @@
+# Reading data
+HPCdata <- read.table("household_power_consumption.txt",skip=1,sep=";")
+#naming the columns and subsetting
+names(HPCdata) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+SubHPCdata <- subset(HPCdata,HPCdata$Date=="1/2/2007" | HPCdata$Date =="2/2/2007")
+
+# Transforming the Date and Time vars from characters into objects of type Date and POSIXlt 
+SubHPCdata$Date <- as.Date(SubHPCdata$Date, format="%d/%m/%Y")
+SubHPCdata$Time <- strptime(SubHPCdata$Time, format="%H:%M:%S")
+SubHPCdata[1:1440,"Time"] <- format(SubHPCdata[1:1440,"Time"],"2007-02-01 %H:%M:%S")
+SubHPCdata[1441:2880,"Time"] <- format(SubHPCdata[1441:2880,"Time"],"2007-02-02 %H:%M:%S")
+
+
+# calling the basic plot function
+plot(SubHPCdata$Time,as.numeric(as.character(SubHPCdata$Global_active_power)),type="l",xlab="",ylab="Global Active Power (kilowatts)") 
+
+# annotating graph
+title(main="Global Active Power Vs Time")
+
